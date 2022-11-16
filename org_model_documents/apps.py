f88e74897a8e2e11d2efd11a87ddae69ae167f86
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-11-16 11:13:02 +0800

    Added code to throw error at startup if required setting is not specified.

--- org_model_documents/apps.py
+++ org_model_documents/apps.py
@@ -1,7 +1,18 @@
 from django.apps import AppConfig
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 
 class OrgModelDocumentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "org_model_documents"
     verbose_name = "Org Model Documents"
+
+    def ready(self):
+        if (
+            not hasattr(settings, "ORG_MODEL_DOCUMENTS_MEDIA_ROOT")
+            or not settings.ORG_MODEL_DOCUMENTS_MEDIA_ROOT
+        ):
+            raise ImproperlyConfigured(
+                "ORG_MODEL_DOCUMENTS_MEDIA_ROOT is not set in settings.py"
+            )
